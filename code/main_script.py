5fe285eb27f7556266cc830ee7c13ced3b021407
AGENT-047 <abhijeetpal743@gmail.com>
2026-01-14 13:37:51 +0000

    batch processing of dimension tables completed

---
+++ code/main_script.py
@@ -0,0 +1,314 @@
+import os
+import shutil
+from pyspark.sql import SparkSession, Window
+from pyspark.sql.functions import col,lit,when,row_number
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType,TimestampType
+import sys
+
+# os.environ["INPUT_DIR"] = "/opt/data/input/user"
+# os.environ["ARCHIVE_DIR"] = "/opt/data/archive/user"
+# os.environ["WAREHOUSE_DIR"] = "/opt/data/warehouse/user"
+
+
+
+WAREHOUSE_DIR = "/opt/data/warehouse"
+
+
+
+
+
+
+def get_spark_session():
+    """
+    Configures Spark to use the Local Filesystem (Host Machine) as the Warehouse
+    """
+    try:
+        SparkSession.getActiveSession().stop()
+    except:
+        pass
+
+    # 2. Define a custom catalog name (e.g., 'local_iceberg')
+    catalog_name = "local_iceberg"
+
+    return SparkSession.builder \
+        .appName("HostWriteIceberg") \
+        .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions") \
+        \
+        .config("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkCatalog") \
+        .config("spark.sql.catalog.spark_catalog.type", "hadoop") \
+        .config("spark.sql.catalog.spark_catalog.warehouse", WAREHOUSE_DIR) \
+         \
+        .config(f"spark.sql.catalog.{catalog_name}", "org.apache.iceberg.spark.SparkCatalog") \
+        .config(f"spark.sql.catalog.{catalog_name}.type", "hadoop") \
+        .config(f"spark.sql.catalog.{catalog_name}.warehouse", WAREHOUSE_DIR) \
+        \
+        .config("spark.sql.defaultCatalog", catalog_name) \
+        .getOrCreate()
+
+def user_dimensional_table_ingestion(param_table_name):
+
+    # INPUT_DIR = "/opt/data/input/user"
+    # ARCHIVE_DIR = "/opt/data/archive/user"
+    # TABLE_NAME = "user_table"
+
+    INPUT_DIR = f"/opt/data/input/{param_table_name}"
+    ARCHIVE_DIR = f"/opt/data/archive/{param_table_name}"
+    TABLE_NAME = f"{param_table_name}_table"
+
+    spark = get_spark_session()
+
+#     spark.sql(f"""  
+#     CALL spark_catalog.system.expire_snapshots(
+#     table => '{TABLE_NAME}',
+#     older_than => TIMESTAMP '2026-01-01 00:00:00',
+#     retain_last => 1
+# )
+#     """)
+    # sys.exit(0)
+
+    
+    spark.sql(f"""
+        delete from {TABLE_NAME} where country_code='IN'
+    """)
+
+
+    
+    # Check for files
+    if not os.path.exists(INPUT_DIR):
+        print(f"Error: Input directory {INPUT_DIR} not found inside container.")
+        return
+
+    files = [f for f in os.listdir(INPUT_DIR) if f.endswith(".json")]
+    
+    if not files:
+        print("No json files found in input_data folder.")
+        return
+
+    print(f"Processing {len(files)} file(s)...")
+
+    # 2. Read Data
+    schema = StructType([
+    StructField("user_id", StringType(), False),
+    StructField("created_at", TimestampType(), True),
+    StructField("country_code", StringType(), True),
+    StructField("device", 
+                StructType([
+                        StructField("os", StringType(), True),
+                        StructField("version", IntegerType(), True)]) , True),
+    StructField("_corrupt_record", StringType(), True) # Field to store corrupt records
+])
+
+    raw_df1 = spark.read.json("file://" +INPUT_DIR)
+    raw_df1.show(5)
+
+    raw_df = spark.read.option("header", "true") \
+    .schema(schema) \
+    .option("mode", "PERMISSIVE") \
+    .option("columnNameOfCorruptRecord", "_corrupt_record") \
+    .json(INPUT_DIR)
+
+    raw_df.persist()
+    raw_df.show()
+    print(f"raw_df count is {raw_df.count()} and filtered raw_df count is {raw_df.filter(col('_corrupt_record').isNull()).count()}")
+    raw_df.unpersist()
+    # sys.exit(0)
+
+    raw_df=raw_df.filter(col("_corrupt_record").isNull())
+    raw_df=raw_df.withColumn("country_code",when(col("country_code").isNull(),lit("UNKNOWN")).otherwise(col("country_code")))
+
+    # 3. Deduplicate (Keep latest created_date per ID)
+    window_spec = Window.partitionBy("user_id").orderBy(col("created_at").desc_nulls_last())
+    deduped_df = raw_df.withColumn("rn", row_number().over(window_spec)) \
+                       .filter("rn == 1").drop("rn")
+    
+
+    temp_table_name=f"incoming_data_{table_name}"
+    deduped_df.createOrReplaceTempView(temp_table_name)
+
+
+
+    # 4. Create Iceberg Table (if not exists)
+    # This creates the folder structure inside ./warehouse on your Host
+  
+
+    spark.sql(f"""
+        CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
+            user_id STRING,
+            created_at TIMESTAMP,
+            country_code STRING,
+            device STRUCT<os: STRING, version: INT>
+        ) USING iceberg
+    """)
+
+
+    spark.sql(f"""
+        MERGE INTO {TABLE_NAME} target
+        USING {temp_table_name} source
+        ON target.user_id = source.user_id
+        WHEN MATCHED AND source.created_at > target.created_at THEN
+            UPDATE SET *
+        WHEN NOT MATCHED THEN
+            INSERT *
+    """)
+    
+    print("Merge Complete. Data written to Host ./warehouse folder.")
+
+    # 6. Archive Files (Move from Input to Archive)
+    # for file in files:
+    #     src = os.path.join(INPUT_DIR, file)
+    #     dst = os.path.join(ARCHIVE_DIR, file)
+        
+    #     # Move the file
+    #     shutil.move(src, dst)
+    #     print(f"Moved {file} to archive.")
+
+
+
+def countries_dimensional_table_ingestion(param_table_name):
+
+    INPUT_DIR = f"/opt/data/input/{param_table_name}"
+    ARCHIVE_DIR = f"/opt/data/archive/{param_table_name}"
+    TABLE_NAME = f"{param_table_name}_table"
+
+    spark = get_spark_session()
+
+
+    
+    # spark.sql(f"""
+    #     delete from {TABLE_NAME} where country_code='IN'
+    # """)
+
+    # sys.exit(0)
+
+    
+    # Check for files
+    if not os.path.exists(INPUT_DIR):
+        print(f"Error: Input directory {INPUT_DIR} not found inside container.")
+        return
+
+    files = [f for f in os.listdir(INPUT_DIR) if f.endswith(".csv")]
+    
+    if not files:
+        print("No json files found in input_data folder.")
+        return
+
+    print(f"Processing {len(files)} file(s)...")
+
+    # 2. Read Data
+    schema = StructType([
+    StructField("country_code", StringType(), False),
+    StructField("country_name", StringType(), True),
+    StructField("_corrupt_record", StringType(), True) # Field to store corrupt records
+])
+
+    raw_df1 = spark.read.csv("file://" +INPUT_DIR)
+    raw_df1.show(5)
+
+    raw_df = spark.read.option("header", "true") \
+    .schema(schema) \
+    .option("mode", "PERMISSIVE") \
+    .option("columnNameOfCorruptRecord", "_corrupt_record") \
+    .csv(INPUT_DIR)
+
+    raw_df.persist()
+    raw_df.show()
+    print(f"raw_df count is {raw_df.count()} and filtered raw_df count is {raw_df.filter(col('_corrupt_record').isNull()).count()}")
+    raw_df.unpersist()
+    # sys.exit(0)
+
+    raw_df=raw_df.filter(col("_corrupt_record").isNull())
+    # raw_df=raw_df.withColumn("country_code",when(col("country_code").isNull(),lit("UNKNOWN")))
+
+    # 3. Deduplicate (Keep latest created_date per ID)
+    # window_spec = Window.partitionBy("user_id").orderBy(col("created_at").desc_nulls_last())
+    # deduped_df = raw_df.withColumn("rn", row_number().over(window_spec)) \
+    #                    .filter("rn == 1").drop("rn")
+
+    deduped_df=raw_df
+    
+    temp_table_name=f"incoming_data_{table_name}"
+    deduped_df.createOrReplaceTempView(temp_table_name)
+
+    # 4. Create Iceberg Table (if not exists)
+    # This creates the folder structure inside ./warehouse on your Host
+  
+
+    spark.sql(f"""
+        CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
+            country_code STRING,
+            country_name STRING
+        ) USING iceberg
+    """)
+
+
+    spark.sql(f"""
+        MERGE INTO {TABLE_NAME} target
+        USING {temp_table_name} source
+        ON target.country_code = source.country_code
+        WHEN MATCHED AND source.country_name <> target.country_name THEN
+            UPDATE SET *
+        WHEN NOT MATCHED THEN
+            INSERT *
+    """)
+    
+    print("Merge Complete. Data written to Host ./warehouse folder.")
+
+    print("showing data.")
+
+    spark.sql(f"""
+        select * from {TABLE_NAME} 
+    """).show(truncate=False)
+
+    # 6. Archive Files (Move from Input to Archive)
+    # for file in files:
+    #     src = os.path.join(INPUT_DIR, file)
+    #     dst = os.path.join(ARCHIVE_DIR, file)
+        
+    #     # Move the file
+    #     shutil.move(src, dst)
+    #     print(f"Moved {file} to archive.")
+
+
+def events_stream_ingestion():
+    print("starting stream")
+
+if __name__ == "__main__":
+
+    job_type=sys.argv[1]
+    table_name=sys.argv[2]
+
+    print(job_type,table_name)
+    
+
+    # match job_type:
+    #     case "stream":
+    #         print("running stream job")
+    #     case "batch":
+    #         match table_name:
+    #             case "user":
+    #                 print(f"running {table_name} table batch job")
+    #             case "countries":
+    #                 countries_dimensional_table_ingestion(table_name)
+    #                 print(f"running {table_name} table batch job")
+    #             case _:  
+    #                 print(f"Unexpected table_name {table_name}")
+                    
+    #     case _:  
+    #         print(f"Unexpected job_type {job_type}")
+    
+    if job_type == "stream":
+        print("running stream job")
+    elif job_type == "batch":
+        if table_name == "user":
+            print(f"running {table_name} table batch job")
+            user_dimensional_table_ingestion(table_name)  
+
+        elif table_name == "countries":
+            print(f"running {table_name} table batch job")
+            countries_dimensional_table_ingestion(table_name)  
+
+        else:
+            print(f"Unexpected table_name {table_name}")
+    else:
+        print(f"Unexpected job_type {job_type}")
+
